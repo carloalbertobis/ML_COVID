e14b9ce0a7a4f9ef6d289bf5dfa2b235093cae1c
charlieabees <carloalbertobissacco@gmail.com>
2022-11-22 18:07:19 +0100

    update

---
+++ to_run.R
@@ -0,0 +1,34 @@
+rm(list=ls(all.names=TRUE))
+
+#set the directory where the file is saved as the working directory
+if (!require("rstudioapi")) install.packages("rstudioapi")
+thisdir<-setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+thisdir<-setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+
+# script_librerias
+
+
+
+# script_parametros
+
+
+
+
+
+
+
+
+
+
+
+# script_machine_learning
+
+launch_step("script_machine_learning/01_T2_40_clean_vaccines.R") 
+launch_step("script_machine_learning/01_T2_40_clean_vaccines.R") 
+launch_step("script_machine_learning/01_T2_40_clean_vaccines.R") 
+
+
+
+
+
+# execute_RMarkdown
